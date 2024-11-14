cd5a8cce7fa6f2d476330f6b259b7d51a148f0c1
Fabio Mano <fmt.mano@gmail.com>
2024-11-14 17:53:38 +0000

    create mongodb connection

---
+++ src/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectToDatabase = async () => {
+  try {
+    const mongoURI =
+      process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bloqit_db';
+    await mongoose.connect(mongoURI);
+
+    console.log('connected to db');
+  } catch (error) {
+    console.error('db connection error: ', error);
+    process.exit(1); // TODO use json files as db alternative?
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import bloqRoutes from './routes/bloq.routes';
+import { connectToDatabase } from './db';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+connectToDatabase();
 
 app.use(express.json());
 
